04d64734b011c062b797c14ce06a453fe8d810ad
Vadym Verba <71729845+vhyver@users.noreply.github.com>
2022-12-21 14:55:53 +0200

    Basic move

---
+++ My project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void ReceiveDamage(float dmg)
+    {
+        health -= dmg;
+        CheckDeath();
+    }
+    private void CheckDeath()
+    {
+        if(health <= 0) 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void CheckOverHeal()
+    {
+        if(health > maxHealth) 
+        {
+            health = maxHealth;
+        }
+    }
+}
---
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 direction;
+    private Animator animator;
+
+    void Start()
+    {
+       animator = GetComponent<Animator>(); 
+    }
+
+    void Update()
+    {
+        TakeInput();
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        SetAnimation(direction);
+    }
+
+    private void TakeInput()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+    }
+
+    private void SetAnimation(Vector2 direction) 
+    {
+        if(direction != Vector2.zero)
+            animator.SetBool("IsRunning", true);
+        else
+            animator.SetBool("IsRunning", false);
+    }
+
+}
