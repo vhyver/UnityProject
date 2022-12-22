f49ffdc6d9f110e7885d97dc93070b029c6aaeb8
Vadym Verba <71729845+vhyver@users.noreply.github.com>
2022-12-23 00:02:23 +0200

    Finishing?

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerMovement : MonoBehaviour
